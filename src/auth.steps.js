f661cc17dd502de1b6f0021d9c6c269f99fb65d3
tajwarul23 <tajwarulchowdhury@gmail.com>
2022-04-10 00:51:39 +0600

    firebase-init

---
+++ src/auth.steps.js
@@ -0,0 +1,17 @@
+/*
+1.create a new project in console.firebase.google.com
+2.npm install firebase
+3.Create firebase.init.js(copy paste) and import getAuth so that we can use  it and export default auth
+4.Go to console>authentication>get started 
+
+
+
+
+*/
+
+/* Hosting Steps
+1.npm install -g firebase -tools
+2.firebase login
+3.firebase init
+4.firebase deploy
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/getAuth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1f1fCdsBpC35Sa2FSM9KrU73F_zXrpTM",
+  authDomain: "ema-john-simple-49e2b.firebaseapp.com",
+  projectId: "ema-john-simple-49e2b",
+  storageBucket: "ema-john-simple-49e2b.appspot.com",
+  messagingSenderId: "506943509750",
+  appId: "1:506943509750:web:165abd99bb275d2bca08d7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth =getAuth(app)
+export default auth;
